58b49b5156e59ad7fdcb88280f5a8ffbebd8b23c
magzimko <35074205+magzimko@users.noreply.github.com>
2018-01-07 21:19:02 +0600

    Create puppetmaster_install.sh

---
+++ puppetmaster_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+curl -O https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
+dpkg -i puppetlabs-release-pc1-xenial.deb
+apt-get update
+echo =============================================
+echo We're going to install puppet master
+echo =============================================
+    apt-get install puppetserver
+echo =============================================
+COMMAND=nope
+while [ "$COMMAND" != "end" ]
+do
+    echo =============================================
+    echo Now run client.sh on client device. Press [ENTER] to complete authorization, when it will be asked from client.
+    echo =============================================
+        COMMAND=`read`
+    echo =============================================
+    echo Client device authorization
+    echo =============================================
+    puppet cert sign --all
+done  
